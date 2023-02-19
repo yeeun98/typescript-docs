f2da42443177c04cf3919f8fc7f29a8432708e17
yeeun-ii <qjspdl@hs.ac.kr>
2023-02-19 22:59:47 +0900

    #2 generic study
    기존 타입 선언 방식의 단점 확인
    유니온 타입 선언 방식의 단점 확인

--- learn-typescript/class-note/8_generics.ts
+++ learn-typescript/class-note/8_generics.ts
@@ -1,14 +1,35 @@
+
 function logText(text) {
     console.log(text);
     return text;
 }
+logText('a')
 
-logText('안농!');  // 문자열 : 안뇽!
-logText(10);       // 숫자   : 10
-logText(true);     // 진위값 : true
+function logNumber(number: number) {
+    console.log(number);
+    return number;
+}
+const num = logNumber(10);
 
-function logText2<T>(text: T):T {
+// 기존 타입 정의 방식은 타입에 따라 동일한 함수를 중복 선언해주어야 한다는 단점이 있어
+// 이를 보완하기 위해 아래 유니온 방식으로 이 점을 해결
+
+function logAny(any: string | number) {
+    console.log(any);
+    any.split("");
+    return any;
+}
+logAny('any function')
+logAny(10);
+
+// 유니온 방식은 선언해놓은 타입들이 다르다보니 함수 내에서 타입 API를 사용할 수 없다는 점이 단점
+// 위 두 방식의 단점을 해결하고자 제네릭 방식을 사용함
+
+function logGeneric<T>(text: T): T{
     console.log(text);
     return text;
 }
-logText2<string>('하이');
+const str = logGeneric<string>('yeeun');
+str.split('');
+const login = logGeneric<boolean>(true);
+login.valueOf();
